1ced1b3ff96d5df735327a70457c0877bf808d12
Christian Mulato <chmulato@gmail.com>
2020-12-02 09:22:51 -0300

    Deletar IV

--- index.js
+++ index.js
@@ -41,9 +41,10 @@ const Post = require('./models/Post')
 
     app.get('/deletar/:id', function(req, res){
         Post.destroy({where: {'id': req.params.id}}).then(function(){
-        res.send("Postagem deletada com sucesso!")
-    }).catch(function(erro){
-        res.send("Esta postagem não existe!")
+            res.send("Postagem deletada com sucesso!")
+        }).catch(function(erro){
+            res.send("Esta postagem não existe!")
+        })
     })
 
 app.listen(8081, function(){
