a8ffeeffb6ecc625668059b4ef0813c0ca339bf6
Christian Mulato <chmulato@gmail.com>
2020-12-02 09:18:25 -0300

    Deletar II

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const Post = require('./models/Post')
         res.render(__dirname + "/views/layouts/formulario")
     })
 
-    // rota de cadastro
+    //rota de cadastro
     app.post('/add', function(req, res){
         Post.create({
             titulo: req.body.titulo,
@@ -37,6 +37,14 @@ const Post = require('./models/Post')
         }).catch(function(erro){
             res.send("Houve um erro: " + erro)
         })
+    })
+
+    app.get('/deletar/:id', function(req, res){
+        Post.destroy({where: {'id': req.params.id}})
+    }).then(function(){
+        res.send("Postagem deletada com sucesso!")
+    }).catch(function(erro){
+        res.send("Esta postagem não existe!")
     })
 
 app.listen(8081, function(){
